753a456d2610f6b1fd5aa17783191632f5205fa6
Aibek Katenov <a_katenov@kbtu.kz>
2023-04-12 22:00:26 +0600

    lab9

--- lab8/env/shop_back/api/urls.py
+++ lab8/env/shop_back/api/urls.py
@@ -4,11 +4,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('home/', views.home),                            #done
-    path('api/products/', views.product_all_list),              #done
-    path('api/products/<int:product_id>/', views.get_one_pro),        #done
-    path('api/categories/', views.list_all_cat),                             #done
-    path('api/categories/<int:cat_id>/', views.get_cat),                         #done
+    path('home/', views.home),
+    path('api/products/', views.product_all_list),
+    path('api/products/<int:product_id>/', views.get_one_pro),
+    path('api/categories/', views.list_all_cat),
+    path('api/categories/<int:cat_id>/', views.get_cat),
     path('api/categories/<int:cat_id>/products/', views.get_one_cat),
 
 ]
